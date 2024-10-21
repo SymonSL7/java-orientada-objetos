d7ea7426bc9e5191ead7d34bb8f1df39d6961e64
Symon Lima <symonlima07@gmail.com>
2024-10-21 16:13:17 -0300

    Getter e Setters

---
+++ exemplo-pacotes-3/src/escola/Escola.java
@@ -0,0 +1,17 @@
+package escola;
+
+public class Escola {
+
+    public static void main(String[] args) {
+        
+        Aluno felipe = new Aluno();
+
+        felipe.setNome("Felipe");
+        felipe.setIdade(8);
+        
+        System.out.println("O Aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos");
+
+    }
+
+
+}
